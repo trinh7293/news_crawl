605b99111a7f03e02d870739f5e5e3e759164880
trinh7293 <trinh7293@gmail.com>
2018-11-28 22:05:29 +0700

    remove pycache from git

--- news/settings.py
+++ news/settings.py
@@ -14,7 +14,8 @@ BOT_NAME = 'news'
 SPIDER_MODULES = ['news.spiders']
 NEWSPIDER_MODULE = 'news.spiders'
 
-
+# FEED_URI = 'data/%(name)s/%(time)s.json'
+# FEED_FORMAT = 'json'
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'news (+http://www.yourdomain.com)'
 
